ec8e32c28e19b061f2c8807ea40a6a9502e15364
sauna1137 <subscription.search@gmail.com>
2024-06-19 18:41:59 +0900

    Initial Commit

---
+++ GithubSearch/GithubSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSearchApp.swift
+//  GithubSearch
+//
+//  Created by KS on 2024/06/19.
+//
+
+import SwiftUI
+
+@main
+struct GithubSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
